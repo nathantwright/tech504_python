384aba484e0649b171a3ba982d043f35c5927fb1
Nathan Wright <nxw105@student.bham.ac.uk>
2025-04-17 13:55:25 +0100

    Change waiter implementation

--- learn_python/day_3_tasks/waiter_helper.py
+++ learn_python/day_3_tasks/waiter_helper.py
@@ -14,55 +14,64 @@ desserts = ({"name": "Brownie", "price": 10.0},
 
 customer_ordered_list = []
 
-print("Hello User! \nThese are the available "
-      "starters:")
+customer_count = int(input("Hello User! \nHow many"
+                           " are in your party? "))
+print("These are the available starters:")
 for option in starters:
     print("- " + option["name"])
 
-chosen_starter = ""
-successful = False
-while not successful:
-    chosen_starter = input("Which starter would "
-                           "you like? ")
-    for s in starters:
-        if s["name"] == chosen_starter:
-            chosen_starter = s
-            successful = True
-print("\nExcellent Choice!")
-customer_ordered_list.append(chosen_starter)
+for c in range(customer_count):
+    chosen_starter = ""
+    successful = False
+    while not successful:
+        chosen_starter = input(f"Customer #{c+1}, "
+                               f"which starter "
+                               f"would you like? ")
+        for s in starters:
+            if s["name"] == chosen_starter:
+                chosen_starter = s
+                successful = True
+    print("\nExcellent Choice!")
+    customer_ordered_list.append(chosen_starter)
 
 print("\nThese are the available mains:")
 for option in mains:
     print("- " + option["name"])
 
-chosen_main = ""
-successful = False
-while not successful:
-    chosen_main = input("Which main would "
-                           "you like? ")
-    for m in mains:
-        if m["name"] == chosen_main:
-            chosen_main = m
-            successful = True
-print("\nWonderful!")
-customer_ordered_list.append(chosen_main)
+for c in range(customer_count):
+    chosen_main = ""
+    successful = False
+    while not successful:
+        chosen_main = input(f"Customer {c+1}, "
+                            f"which main would "
+                            f"you like? ")
+        for m in mains:
+            if m["name"] == chosen_main:
+                chosen_main = m
+                successful = True
+    print("\nWonderful!")
+    customer_ordered_list.append(chosen_main)
 
 print("\nThese are the available desserts:")
 for option in desserts:
     print("- " + option["name"])
 
-chosen_dessert = ""
-successful = False
-while not successful:
-    chosen_dessert = input("Which dessert would "
-                           "you like? ")
-    for d in desserts:
-        if d["name"] == chosen_dessert:
-            chosen_dessert = d
-            successful = True
-print("\nSounds good!")
-customer_ordered_list.append(chosen_dessert)
+for c in range(customer_count):
+    chosen_dessert = ""
+    successful = False
+    while not successful:
+        chosen_dessert = input(f"Customer {c+1}, "
+                               f"which dessert "
+                               f"would you like? ")
+        for d in desserts:
+            if d["name"] == chosen_dessert:
+                chosen_dessert = d
+                successful = True
+    print("\nSounds good!")
+    customer_ordered_list.append(chosen_dessert)
 
-print(f"You ordered {chosen_starter['name']}, {chosen_main['name']}, and {chosen_dessert['name']}.")
-price = chosen_starter["price"] + chosen_main["price"] + chosen_dessert["price"]
+print(f"You ordered {customer_ordered_list}.")
+price = 0.0
+for o in customer_ordered_list:
+    price += o["price"]
 print(f"The total cost is £{price: .2f}")
